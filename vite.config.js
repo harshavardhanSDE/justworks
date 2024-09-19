f873fdbe554fdb3f8f52c12e212b1997dd9f3d14
harsavardhanSDE <harshavardhan.workspace@gmail.com>
2024-09-20 02:12:42 +0530

    added

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'; 
+import react from '@vitejs/plugin-react';
+import copy from 'rollup-plugin-copy'; 
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+
+  // CSS: SCSS
   css: {
     preprocessorOptions : {
       scss: {
@@ -12,5 +15,33 @@ export default defineConfig({
         outputStyle: 'compressed',
       }
     }
+  }, 
+
+
+  // Rollup for copying and custom naming built files.
+
+  build: {
+    rollupOptions: {
+      plugins: [
+        copy({
+          // targets: [
+          //   {
+            
+          //   }
+          // ], 
+
+        hook: 'writeBundle'
+        }),
+      ],
+
+
+      // output
+
+      output: {
+        entryFileNames: 'assets/overlay.js',
+      }
+    }
   }
+
+
 })
